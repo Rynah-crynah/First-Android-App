beea8f5898dcd3c8dbf5b4aee99d532246806b50
Rynah-Crynah <swabrinyusufshamun@gmail.com>
2022-05-08 20:00:20 +0300

    Money-Transfer

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.myapplication
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    lateinit var btnBmi:Button
+    lateinit var btnSendMoney:Button
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        btnBmi = findViewById(R.id.btnBmi)
+        btnBmi.setOnClickListener {
+            val intent = Intent(this,BMICalculatorActivity::class.java)
+            startActivity(intent)
+        }
+        btnSendMoney = findViewById(R.id.btnSendMoney)
+        btnSendMoney.setOnClickListener {
+            val intent = Intent(this,SendMoney::class.java)
+            startActivity(intent)
+        }
+    }
+}
